977b9f852ec33d2b17539b712bfe8321f06bc0db
arieltecay <arieltecay@gmail.com>
2022-06-02 20:10:44 -0300

    update clase

--- js/operaciones.js
+++ js/operaciones.js
@@ -1,20 +1,23 @@
-import mostrarCartel, { nombre, edad as e, sumar, Persona } from './suma.js'
-
-console.log(nombre);
-console.log(e);
+import { sumar } from './suma.js'
 
 const btnSumar = document.querySelector('#sumar')
 const input1 = document.querySelector('#n1')
 const input2 = document.querySelector('#n2')
 
 
-btnSumar.addEventListener('click', operacion)
+btnSumar.addEventListener('click', mostrarHTML)
 
 function operacion() {
-    console.log(sumar(input1.value, input2.value));
+    return sumar(parseInt(input1.value), parseInt(input2.value))
 }
 
-const persona = new Persona('Ariel')
-console.log(persona.mostrarInfo());
+function mostrarHTML() {
+    const resultado = document.querySelector('#resultado')
+    let resultado2 = operacion()
+    let texto = document.createElement('h1')
+    texto.innerText = resultado2
+    resultado.appendChild(texto)
+    /*     const suma = operacion()
+        resultado.textContent = suma */
+}
 
-mostrarCartel()
